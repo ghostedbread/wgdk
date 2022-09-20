c472913742ba6990c24d0e7de59cd9bd4a00a878
ghostedbread <105466373+ghostedbread@users.noreply.github.com>
2022-09-19 19:39:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,146 @@
-// wgdk-js 1.0 //
-// this version doesn't have the fancy stuff on the readme, still working on the features because I suck at js :/
-function loadFile(filePath) { // copied code from stackoverflow :D
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status == 200) {
-    result = xmlhttp.responseText;
-  }
-  return result;
+// Game Web Development Kit - Version 1.0
+// Function stuff
+function CheckIfVarIsCertainClass(v,classname) {
+    let result;
+    if (typeof v == "function") {
+        let parsedstring = v.toString();
+        if (parsedstring.replace(/\[|\]/,"").replace("class","").replace(" ",""))
+            result = true
+        else
+            result = false;
+    } else {
+        result = false;
+    }
+    if (!result)
+        return Error("Result of function \"CheckIfVarIsCertainClass()\" is null or undefined.");
+    return result;
 }
-function Initialize(options) { // optional options parameter incase if wgdk.json doesn't exist, if both dont exist, it returns an error.
-  var loc = window.location.pathname;
-  var dir = loc.substring(0, loc.lastIndexOf('/')); // another copy of code from stackoverflow :/
-  if (!options) {
-     let json = loadFile(dir)
-     // don't know how to check if its json yet so screw it :P
-     let parsedjson = JSON.parse(JSON.stringify(json))
-     
-  }
+// Vector stuff
+class Vector2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+        forward = () => { return new Vector2(this.x, this.y+1) }
+
+        down = () => { return new Vector2(this.x, this.y-1) }
+
+        left = () => { return new Vector2(this.x-1, this.y) }
+
+        right = () => { return new Vector2(this.x+1, this.y) }
+}
+class Vector3 {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    forward = () => { return new Vector3(this.x, this.y, this.z+1) }
+    backward = () => { return new Vector3(this.x, this.y, this.z-1) }
+    left = () => { return new Vector3(this.x-1, this.y, this.z) }
+    right = () => { return new Vector3(this.x+1, this.y, this.z) }
+    up = () => { return new Vector3(this.x, this.y+1, this.z) }
+    down = () => { return new Vector3(this.x, this.y-1, this.z) }
+}
+
+function getmouseposincanvas(canvas) {
+    let returnedx;
+    let returnedy;
+    function cnvs_getCoordinates(e) {
+        x = e.clientX;
+        y = e.clientY;
+        returnedx = x;
+        returnedy = y;
+    }
+
+    function cnvs_clearCoordinates() {
+        document.getElementById("xycoordinates").innerHTML = "";
+        returnedx = "";
+        returnedy = "";
+    }
+    let typecanvas = `${typeof canvas}`;
+    if (typecanvas[0].toLowerCase == "a" || "e" || "i" || "o" || "u") { // grammar lol
+        `an ${typeof canvas}`
+    } else {
+        typecanvas = `a ${typeof canvas}`
+    }
+    if (typeof canvas == HTMLCanvasElement) {
+        return new Vector2(returnedx,returnedy)
+    } else {
+        console.error(`Error from function \"getmouseposincanvas()\": parameter \"canvas\" is not a HTMLCanvasElement. Instead it is ${typecanvas}.`)
+    }
+}
+class Key {
+    constructor(keycode, callback) {
+        this.keycode = keycode;
+        this.callback = callback;
+    }
+}
+class KeyDefinition {
+    constructor(keys) {
+        this.keys = keys;
+    }
+}
+let Input = {
+    InputInitalizer(keyDefinition) {
+        if (typeof keyDefinition == 'object') {
+            for (let key in keyDefinition.keys) {
+                if (focusedElement) {
+                    let focusedElement = keyDefinition.focusedElement;
+                    let usingfocusedElement = true;
+                    let keyname = keyDefinition.keys[key].keycode;
+                    let keyfunc = keyDefinition.keys[key].callback;
+                    document.addEventListener("keydown", (e) => {
+                        if (e.code == keyname) {
+                            keyfunc()
+                        }
+                    })
+                }
+            }
+        } else if (typeof keyDefinition == 'function') {
+            if (CheckIfVarIsCertainClass(keyDefinition,"KeyDefinition")) {
+                
+            }
+        }
+    }
+}
+function InitializeSprite(dimension, sprite) {
+    if (dimension == "2D") {
+        sprite.position = new Vector2(0,0)
+        sprite.size = {
+            width: 0,
+            height: 0
+        }
+        sprite.color = {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 1
+        };
+        sprite.rotation = 0;
+        sprite.velocity = {
+            x: 0,
+            y: 0
+        };
+        sprite.gravity = 1
+        sprite.friction = 0;
+    } else if (dimension == "3D") {
+        sprite.position = {
+            x: 0,
+            y: 0,
+            z: 0
+        };
+        sprite.size = {
+            width: 0,
+            height: 0,
+            depth: 0
+        }
+        sprite.color = {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 1
+        };
+    }
+}
+
